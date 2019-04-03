42a9a0bdc283a2b0ac7b4759216c8c9e5af9f30a
UNIZA\uniza.lin <unizalin@gmail.com>
2019-04-03 11:29:41 +0800

    產生隨機密碼

--- generate_password.js
+++ generate_password.js
@@ -1,6 +1,12 @@
+// define sample function to randomly return a item in an array
+function sample(array) {
+  const index = Math.floor(Math.random() * array.length);
+  return array[index];
+}
+
 // define generatePassword function
 function generatePassword() {
-  console.log('This function will generate password');
+  // console.log('This function will generate password');
   // define things user might want
   const lowerCaseLetters = 'abcdefghiklmnopqrstuvwxyz';
   const upperCaseLetters = lowerCaseLetters.toUpperCase();
@@ -15,40 +21,44 @@ function generatePassword() {
     numbers: 'on',
     excludeCharacters: '40'
   };
-  console.log('options', options);
 
   // create a collection to store things user picked up
-  let collection = [];
+  let collection = []
+
   if (options.lowercase === 'on') {
-    collection = collection.concat(lowerCaseLetters.split(''));
+    collection = collection.concat(lowerCaseLetters.split(''))
   }
+
   if (options.uppercase === 'on') {
-    collection = collection.concat(upperCaseLetters.split(''));
+    collection = collection.concat(upperCaseLetters.split(''))
   }
+
   if (options.numbers === 'on') {
-    collection = collection.concat(numbers.split(''));
+    collection = collection.concat(numbers.split(''))
   }
+
   if (options.symbols === 'on') {
-    collection = collection.concat(symbols.split(''));
+    collection = collection.concat(symbols.split(''))
   }
+
 
   // remove things user do not need
   if (options.excludeCharacters) {
-    console.log(`exclude characters: ${options.excludeCharacters}`)
-    collection = collection.filter(character => !options.excludeCharacters.includes(character)
-      //判斷collection 是否含有 character
-      //有的話回傳true，但因為需剔除掉，須回傳false ，加上 !
-
-      // if the character includes in 'excludeCharacters',
-      // return false to remove the character in the collection
-    );
+    //判斷collection 是否含有 character
+    //有的話回傳true，但因為需剔除掉，須回傳false ，加上 !
+    collection = collection.filter(character => !options.excludeCharacters.includes(character));
   }
-  console.log('collection' + collection);
+
   // start generating password
+  let password = '';
+  for (let i = 0; i < options.length; i++) {
+    password += sample(collection);
+  }
 
   // return the generated password
+  console.log('password', password);
+  return password;
 
 }
 
-// invoke generatePassword function 
 generatePassword();
